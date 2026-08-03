7ea10368d8129f970be13e75c71f95bc204127d9
Juan Sierra <ing.juan.sierra.sm@gmail.com>
2026-08-02 21:48:32 -0500

    chore: initialize Spring Boot project

---
+++ src/main/java/com/juansierrasegurosbolivar/gestion_polizas/GestionPolizasApplication.java
@@ -0,0 +1,13 @@
+package com.juansierrasegurosbolivar.gestion_polizas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPolizasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPolizasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juansierrasegurosbolivar/gestion_polizas/GestionPolizasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juansierrasegurosbolivar.gestion_polizas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPolizasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
